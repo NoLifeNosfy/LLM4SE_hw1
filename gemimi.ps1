69c38582739302d4585b1617c60ef61fde1e4b12
HanBoqiao <hanbohbq@smail.nju.edu.cn>
2025-09-27 16:36:35 +0800

    release-v2.0

--- gemimi.ps1
+++ gemimi.ps1
@@ -1,5 +1,3 @@
 $env:https_proxy="http://127.0.0.1:7890"
 $env:GOOGLE_CLOUD_PROJECT="imperial-data-472503-v0"
-# $env:GOOGLE_CLOUD_PROJECT="buoyant-nectar-472503-s3"
-# $env:GEMINI_API_KEY="AIzaSyC07ARCiNlOOp_dhjZ47faj3Fj52Fz5fhs"
 gemini
